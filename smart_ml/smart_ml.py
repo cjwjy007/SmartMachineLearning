5d9626a1d7c593ca5edbbe4d38ad4ef914af3e73
wei <317694683@qq.com>
2017-06-01 18:51:29 +0800

    SmartMachineLearning

--- smart_ml/smart_ml.py
+++ smart_ml/smart_ml.py
@@ -2,7 +2,7 @@ import random
 
 import pandas as pd
 import numpy as np
-from pandas import Series
+
 from sklearn import preprocessing
 from sklearn.model_selection import cross_val_score
 from sklearn.ensemble import AdaBoostClassifier
@@ -16,6 +16,7 @@ from sklearn.feature_selection import SelectFromModel
 import sys
 from io import StringIO
 
+# from auto_ml import *
 pd.options.mode.chained_assignment = None
 
 
@@ -31,7 +32,7 @@ class Stdin:
         sys.stdin = old_stdin
 
     @staticmethod
-    def get_input(text):
+    def get_int_input(text):
         try:
             x = int(input(text))
         except ValueError as e:
@@ -70,21 +71,34 @@ class SmartML:
         else:
             return False
 
-    def auto_learn(self):
-        # columns info
-        columns_list = self.train_df.columns.values.tolist()
-        self.print_columns(columns_list)
+    def auto_learn(self, preprocessed_df=None):
 
-        # pick predict column
-        y_index = self.pick_y(columns_list)
-        y_column = self.train_df.iloc[:, y_index]
-        self.train_df.drop(self.train_df.columns[y_index], axis=1, inplace=True)
+        if preprocessed_df is None:
+            # columns info
+            columns_list = self.train_df.columns.values.tolist()
+            self.print_columns(columns_list)
 
-        # preprocess Y
-        preprocessed_y = self.data_y_preprocessing(train_columns=self.train_df, pre_column=y_column, is_auto=True)
+            # pick predict column
+            y_index = self.pick_y(columns_list)
 
-        # preprocess X
-        preprocessed_x = self.data_x_preprocessing(train_columns=self.train_df, pre_column=y_column, is_auto=True)
+            # preprocess Y
+            self.data_y_preprocessing(train_columns=self.train_df, y_index=y_index, is_auto=True)
+
+            # extract Y column
+            preprocessed_y = self.train_df.iloc[:, y_index]
+            self.train_df.drop(self.train_df.columns[y_index], axis=1, inplace=True)
+
+            # preprocess X
+            preprocessed_x = self.data_x_preprocessing(train_columns=self.train_df, pre_column=preprocessed_y,
+                                                       is_auto=True)
+
+            # save the Dataframe as csv
+            pd.concat([preprocessed_y, preprocessed_x], axis=1).to_csv("preprocessed_df.csv")
+        else:
+            # get x and y
+            preprocessed_y = preprocessed_df.iloc[:, 1]
+            preprocessed_x = preprocessed_df.drop(preprocessed_df.columns[0], axis=1)
+            preprocessed_x = preprocessed_x.drop(preprocessed_x.columns[0], axis=1)
 
         # train data
         best_model_name, best_model, best_score = self.select_best_model(data_x=preprocessed_x, data_y=preprocessed_y,
@@ -92,35 +106,55 @@ class SmartML:
         print("best_model_name: " + best_model_name)
         print("best_score： " + str(best_score))
 
-    def data_y_preprocessing(self, train_columns, pre_column, is_auto):
-        if self.is_categorical_data(pre_column.dtype):
-            pre_column = pd.Series(preprocessing.LabelEncoder().fit_transform(pre_column),
-                                   name=pre_column.name)
-        return pre_column
+    def data_y_preprocessing(self, train_columns, y_index, is_auto):
+        self.transform_y(train_df=train_columns, index_y=y_index)
+        self.data_encoding_y(train_df=train_columns, index_y=y_index)
+
+    def transform_y(self, train_df, index_y):
+        column_y = train_df.iloc[:, index_y]
+        values_y = column_y.value_counts()
+        column_y_map = {}
+        for idx, val in enumerate(values_y):
+            x = Stdin.get_int_input("map " + values_y.index[idx] + " to(if input -1,delete column):")
+            if x == -1:
+                column_y_map[values_y.index[idx]] = np.nan
+            else:
+                column_y_map[values_y.index[idx]] = x
+        train_df.iloc[:, index_y] = column_y.map(column_y_map)
+        train_df.dropna(subset=[column_y.name], inplace=True)
+        train_df.index = range(len(train_df))
+
+    def data_encoding_y(self, train_df, index_y):
+        column_y = self.train_df.iloc[:, index_y]
+        if self.is_categorical_data(column_y.dtype):
+            self.train_df.iloc[:, index_y] = pd.Series(preprocessing.LabelEncoder().fit_transform(column_y),
+                                                       name=column_y.name)
 
     def data_x_preprocessing(self, train_columns, pre_column, is_auto):
-        train_columns = self.feature_pre_selection(train_columns, is_auto)
-        train_columns = self.fill_missing(train_columns, is_auto)
-        train_columns = self.data_encoding(train_columns, is_auto)
-        train_columns = self.feature_selection(train_columns, pre_column)
+        train_columns = self.feature_pre_selection_x(train_columns, is_auto)
+        train_columns = self.fill_missing_x(train_columns, is_auto)
+        train_columns = self.data_encoding_x(train_columns, is_auto)
+        train_columns = self.feature_selection_x(train_columns, pre_column)
         return train_columns
 
-    def feature_pre_selection(self, train_columns, is_auto):
+    def feature_pre_selection_x(self, train_columns, is_auto):
         delete_columns_name = []
         for i in range(0, len(train_columns.columns.values.tolist())):
             column = train_columns.iloc[:, i]
+            column_notnull = column[column.notnull()]
+            if column_notnull.size <= 10:
+                delete_columns_name.append(column_notnull.name)
+                continue
             # categorical
             if self.is_categorical_data(column.dtype):
                 # values are totally different
-                column_notnull = column[column.notnull()]
-                if column_notnull.size == column_notnull.value_counts().values.size:
-                    print("数据 {name}中的值完全不相同，自动删除".format(name=column.name))
+                if column_notnull.value_counts().values.size / column_notnull.size > 0.8:
+                    print("数据 {name}中的值几乎不相同，自动删除".format(name=column.name))
                     delete_columns_name.append(column_notnull.name)
                     continue
             # numerical
             else:
                 # values are totally different
-                column_notnull = column[column.notnull()]
                 if column_notnull.size == column_notnull.value_counts().values.size:
                     if is_auto:
                         if (column_notnull[0] + column_notnull[
@@ -138,7 +172,7 @@ class SmartML:
 
         return train_columns
 
-    def fill_missing(self, train_columns, is_auto):
+    def fill_missing_x(self, train_columns, is_auto):
         delete_columns_name = []
         for i in range(0, len(train_columns.columns.values.tolist())):
             train_column = train_columns.iloc[:, i]
@@ -177,7 +211,7 @@ class SmartML:
                     print("数据参考：")
                     print(train_column.value_counts().head())
                     print(self.handle_missing_value_str)
-                    x = Stdin.get_input('')
+                    x = Stdin.get_int_input('')
 
                 if x == 0:
                     value = input("请输入要填入的数值")
@@ -210,7 +244,7 @@ class SmartML:
             train_columns.drop(delete_column_name, axis=1, inplace=True)
         return train_columns
 
-    def data_encoding(self, train_columns, is_auto):
+    def data_encoding_x(self, train_columns, is_auto):
         encoded_columns = pd.DataFrame()
         for column_name in train_columns.columns.values.tolist():
             train_column = train_columns[column_name]
@@ -222,13 +256,31 @@ class SmartML:
                     else:
                         print("{name}为categorical类型".format(name=train_column.name))
                         print(self.handle_cate_encode_str)
-                        x = Stdin.get_input('')
+                        x = Stdin.get_int_input('')
 
                     if x == 0:
                         encoded_column = pd.Series(preprocessing.LabelEncoder().fit_transform(train_column),
                                                    name=train_column.name)
                         break
                     elif x == 1:
+                        # test if there are too many unique values,cast minority to MINORITY_MARK
+                        MAX_VALUE_COUNTS = 100
+                        MINORITY_MARK = -1
+                        value_counts_dic = train_column.value_counts()
+                        if value_counts_dic.values.size > MAX_VALUE_COUNTS:
+                            min_value_counts = value_counts_dic[MAX_VALUE_COUNTS]
+                            # LabelEncoder to accelerate the loop
+                            train_column = pd.Series(preprocessing.LabelEncoder().fit_transform(train_column),
+                                                     name=train_column.name)
+                            value_counts_dic = train_column.value_counts()
+                            print("column {name} is doing {times} times loop which may take a long time"
+                                  .format(name=train_column.name, times=len(train_column)))
+                            for idx, val in enumerate(train_column):
+                                if value_counts_dic[val] < min_value_counts:
+                                    train_column[idx] = MINORITY_MARK
+
+                        # ont-hot encoder
+                        pass
                         encoded_column = pd.get_dummies(train_column, prefix=train_column.name)
                         break
                     elif x == 2:
@@ -242,7 +294,7 @@ class SmartML:
                     else:
                         print("{name}为numerical类型".format(name=train_column.name))
                         print(self.handle_num_encode_str)
-                        x = Stdin.get_input('')
+                        x = Stdin.get_int_input('')
                     if x == 0:
                         # Scaled data has zero mean and unit variance
                         encoded_column = pd.Series(preprocessing.scale(train_column), name=train_column.name)
@@ -262,7 +314,7 @@ class SmartML:
                 encoded_columns = pd.concat([encoded_columns, train_column], axis=1)
         return encoded_columns
 
-    def feature_selection(self, train_columns, pre_column):
+    def feature_selection_x(self, train_columns, pre_column):
         selector = SelectFromModel(RandomForestClassifier(n_jobs=-1, max_depth=10, n_estimators=15),
                                    threshold='20*mean')
         selector.fit(train_columns, pre_column)
@@ -271,12 +323,12 @@ class SmartML:
         return train_columns
 
     def print_columns(self, cols_list):
-        for i in range(0, len(cols_list)):
-            print(str(i) + ':' + cols_list[i])
+        for idx, val in enumerate(cols_list):
+            print(str(idx) + ':' + val)
 
     def pick_y(self, cols_list):
         while True:
-            y = int(Stdin.get_input("选择要预测的列"))
+            y = int(Stdin.get_int_input("选择要预测的列"))
             if y in range(0, len(cols_list)):
                 break
             else:
@@ -310,7 +362,7 @@ class SmartML:
             else:
                 for i in range(0, len(model_list)):
                     print(str(i) + ":" + model_list[i])
-                x = Stdin.get_input("选择你要使用的模型")
+                x = Stdin.get_int_input("选择你要使用的模型")
             if x in range(0, len(model_list)):
                 return model_list[x], model_map[model_list[x]]
             else:
@@ -342,13 +394,14 @@ class SmartML:
 
 if __name__ == '__main__':
     # input data
-    # old_stdin = input_stdin("0\n1\n4\n2\n2\n0\n0\n2\n2\n1\n0\n1\n3\n")
-    # recover_stdin(old_stdin)
-
+    # old_stdin = Stdin.input_stdin("0\n1\n4\n2\n2\n0\n0\n2\n2\n1\n0\n1\n3\n")
+    # Stdin.recover_stdin(old_stdin)
+    # old_stdin = Stdin.input_stdin("16\n-1\n1\n0\n-1\n-1\n-1\n-1\n1\n-1\n0\n0\n")
 
     # read data
     train_df = pd.read_csv("/Users/wei/PycharmProjects/MachineLearning/Loan/loan.csv")
+    preprocessed_df = pd.read_csv("preprocessed_df.csv")
 
-    # initial autoML
+    # initial SmartML
     smart_ml = SmartML(train_df=train_df)
-    smart_ml.auto_learn()
+    smart_ml.auto_learn(preprocessed_df=preprocessed_df)
